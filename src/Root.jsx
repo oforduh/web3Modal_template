a3a748a3b5e6bab015ce6382db562ceff0fe2a3a
oforduhh <oforduhh@gmail.com>
2022-09-15 14:22:13 +0100

    validated the chain id

--- src/Root.jsx
+++ src/Root.jsx
@@ -1,10 +1,10 @@
 /* eslint-disable global-require */
 import { Component } from "react";
 import { BrowserRouter } from "react-router-dom";
-
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 import App from "./App";
-
 
 export default class Root extends Component {
   constructor(props) {
@@ -13,9 +13,20 @@ export default class Root extends Component {
 
   render() {
     return (
-          <BrowserRouter >
-            <App />
-          </BrowserRouter>
+      <BrowserRouter>
+        <ToastContainer
+          position="top-left"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+        />
+        <App />
+      </BrowserRouter>
     );
   }
 }
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,59 +1,60 @@
-import React, { useCallback, useState } from 'react'
-import styles from "./navbar.module.scss"
+import React, { useCallback, useState } from "react";
+import styles from "./navbar.module.scss";
 import WalletConnectProvider from "@walletconnect/web3-provider";
 import Web3Modal from "web3modal";
 import { StaticJsonRpcProvider, Web3Provider } from "@ethersproject/providers";
-import { EnvHelper } from '../../helper/Environment';
-import CoinbaseWalletSDK from '@coinbase/wallet-sdk';
+import { EnvHelper } from "../../helper/Environment";
+import CoinbaseWalletSDK from "@coinbase/wallet-sdk";
+import { toast } from "react-toastify";
 
 const Navbar = () => {
-
   function getTestnetURI() {
     return "https://data-seed-prebsc-1-s2.binance.org:8545";
   }
-  
+
   function getMainnetURI() {
     return "https://bsc-dataseed.binance.org";
   }
-  
 
-  const [web3Modal, setWeb3Modal] = useState(new Web3Modal({
-    network: "mainnet", // optional
-    cacheProvider: true, // optional
-    providerOptions: { 
-      walletconnect: {
-        package: WalletConnectProvider,
-        options: {
-          rpc: {
-            97: 'https://data-seed-prebsc-1-s2.binance.org:8545'
+  const [web3Modal, setWeb3Modal] = useState(
+    new Web3Modal({
+      network: "mainnet", // optional
+      cacheProvider: true, // optional
+      providerOptions: {
+        walletconnect: {
+          package: WalletConnectProvider,
+          options: {
+            rpc: {
+              97: "https://data-seed-prebsc-1-s2.binance.org:8545",
+            },
+            chainID: 97,
           },
-          chainID: 97
+        },
+        binancechainwallet: {
+          package: true,
+        },
+        coinbasewallet: {
+          package: CoinbaseWalletSDK, // Required
+          options: {
+            appName: "My Awesome App", // Required
+            rpc: {
+              97: "https://data-seed-prebsc-1-s2.binance.org:8545",
+            },
+            chainID: 97,
+          },
         },
       },
-      binancechainwallet: {
-        package: true
-      },
-      coinbasewallet: {
-        package: CoinbaseWalletSDK, // Required
-        options: {
-          appName: "My Awesome App", // Required
-          rpc: {
-            97: 'https://data-seed-prebsc-1-s2.binance.org:8545'
-          },
-          chainID: 97
-        }
-      },
-   
-    },
-  }),);
+    })
+  );
   const [uri, setUri] = useState(getMainnetURI());
   const [chainID, setChainID] = useState(1);
   const [connected, setConnected] = useState(false);
   const [provider, setProvider] = useState(new StaticJsonRpcProvider(uri));
-  const [address, setAddress] = useState("")
+  const [address, setAddress] = useState("");
 
   const _initListeners = useCallback(
-    rawProvider => {
+    (rawProvider) => {
+      console.log(rawProvider);
       if (!rawProvider.on) {
         console.log("Not raw Provider");
         return;
@@ -61,94 +62,95 @@ const Navbar = () => {
       // when wallet account is changed
       rawProvider.on("accountsChanged", async (accounts) => {
         console.log("accounts changed on raw provider");
-        setTimeout(() => window.location.reload(), 1);
+        // setTimeout(() => window.location.reload(), 1);
       });
       // when chainId is changed
       rawProvider.on("chainChanged", async (chain) => {
         _checkNetwork(chain);
         console.log("chain changed on raw Provider");
-        setTimeout(() => window.location.reload(), 1);
+        // setTimeout(() => window.location.reload(), 1);
       });
 
       rawProvider.on("network", (_newNetwork, oldNetwork) => {
         console.log("old network on raw provider raw Provider");
         if (!oldNetwork) return;
-        window.location.reload();
+        // window.location.reload();
       });
     },
-    [provider],
+    [provider]
   );
 
-  const _checkNetwork = (otherChainID) => {  
-    console.error(
-      "You are switching networks",
-      EnvHelper.getOtherChainID(),
-      otherChainID === EnvHelper.getOtherChainID() || otherChainID === 4,
-    );
-    // when chainID is not test-BSC, it will return false and processes are halt.
-    if (chainID !== otherChainID) {
-      console.warn("You are switching networks", EnvHelper.getOtherChainID());
-      if (otherChainID === EnvHelper.getOtherChainID() || otherChainID === 4) {
-        setChainID(otherChainID);
-        otherChainID === EnvHelper.getOtherChainID() ? setUri(getMainnetURI()) : setUri(getTestnetURI());
+  const _checkNetwork = (otherChainID) => {
+    switch (otherChainID) {
+      case 1:
         return true;
-      }
-      return false;
+      default:
+        return false;
     }
-    return true;
   };
-    // connect - only runs for WalletProviders
-    const connect = useCallback(async () => {
-      const rawProvider = await web3Modal.connect();
-      await web3Modal.toggleModal();
-      console.log("This is the raw provider"+ rawProvider);
-      console.log(rawProvider.on);
-      _initListeners(rawProvider)
-      const connectedProvider = new Web3Provider(rawProvider, "any");
+  // connect - only runs for WalletProviders
+  const connect = useCallback(async () => {
+    const rawProvider = await web3Modal.connect();
+    await web3Modal.toggleModal();
+    let liz = _initListeners(rawProvider);
+    console.log(liz);
 
-      const chainId = await connectedProvider.getNetwork().then(network => network.chainId);
-      console.log(chainId);
-      const connectedAddress = await connectedProvider.getSigner().getAddress();
-      console.log(connectedAddress);
-      const validNetwork = _checkNetwork(chainId);
-      console.log(validNetwork);
-      if (!validNetwork) {
-        alert("Wrong network, please switch to bsc testnet")
-        console.error("Wrong network, please switch to bsc mainnet");
-        return;
-      }
+    const connectedProvider = new Web3Provider(rawProvider, "any");
 
-      console.log(validNetwork);
-      setAddress(connectedAddress);
-      setProvider(connectedProvider);
-      setConnected(true);
-      return connectedProvider;
-  
-    }, [ provider, web3Modal,connected]);
+    const chainId = await connectedProvider
+      .getNetwork()
+      .then((network) => network.chainId);
+    console.log(chainId);
+    const validNetwork = _checkNetwork(chainId);
+    if (!validNetwork) {
+      return toast.warn(
+        "Switch to the Etherium mainnet and click connect wallet"
+      );
+    }
 
-    const disconnect = useCallback(async () => {
-      console.log("disconnecting");
-      web3Modal.clearCachedProvider();
-      setConnected(false);
-  
-      setTimeout(() => {
-        window.location.reload();
-      }, 1);
-    }, [provider, web3Modal, connected]);
-  
-    console.log(`This is our address ${address}`);
+    const connectedAddress = await connectedProvider.getSigner().getAddress();
+    console.log(connectedAddress);
+
+    console.log(validNetwork);
+    setAddress(connectedAddress);
+    setProvider(connectedProvider);
+    setConnected(true);
+    return connectedProvider;
+  }, [provider, web3Modal, connected]);
+
+  const disconnect = useCallback(async () => {
+    console.log("disconnecting");
+    web3Modal.clearCachedProvider();
+    setConnected(false);
+
+    setTimeout(() => {
+      window.location.reload();
+    }, 1);
+  }, [provider, web3Modal, connected]);
+
+  let ellipsis = address
+    ? `${address.slice(0, 3)}
+    ...${address.slice(address.length - 5, address.length)}`
+    : "Connect Wallet";
+
   return (
     <div className={styles.container}>
-
-        <div className={styles.navbar_container}>
-            <div>Web3Modal</div>
-            <div><button onClick={()=>{
-            {!connected? connect():disconnect()}
-            }}>{connected?address:"connect wallet"}</button></div>
-
+      <div className={styles.navbar_container}>
+        <div>Web3Modal</div>
+        <div>
+          <button
+            onClick={() => {
+              {
+                !connected ? connect() : disconnect();
+              }
+            }}
+          >
+            {connected ? ellipsis : "connect wallet"}
+          </button>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
